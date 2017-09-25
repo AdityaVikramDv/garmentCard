9d07eb57089e78fa2e268e64ee2b63deba7880c9
Aditya Vikram Dv <adityavikram.dv@gmail.com>
2017-09-25 08:12:19 +0530

    Added two more cards

--- garmentcard/index.js
+++ garmentcard/index.js
@@ -5,6 +5,10 @@ import App from './components/App.jsx';
 
 ReactDOM.render(
 <div className='container'>
+<App title='T shirts' cost = '999.00' sizesAvailable='XXL,XL,L,M,S,XS' defaultSizeSelected='M' url="http://shimplypageimage.s3.amazonaws.com/47489/original.?1427954604" />
+	
+	<App title='T shirts' cost = '999.00' sizesAvailable='XXL,XL,L,M,S,XS' defaultSizeSelected='M' url="http://shimplypageimage.s3.amazonaws.com/47489/original.?1427954604" />
+	
 	<App title='T shirts' cost = '999.00' sizesAvailable='XXL,XL,L,M,S,XS' defaultSizeSelected='M' url="http://shimplypageimage.s3.amazonaws.com/47489/original.?1427954604" />
 	<App title='Jeans' cost = '3999.00' sizesAvailable='XXXL,XXL,XL,L,M,S,XS' defaultSizeSelected='L' url="https://s-media-cache-ak0.pinimg.com/originals/41/e7/78/41e778be843780c57672de82a24e5799.jpg" />
 	<App title='Shirts & Jackets' cost = '7999.00' sizesAvailable='XXL,XL,L,M,S,XS' defaultSizeSelected='XL' url="http://levistrauss.com/wp-content/uploads/2015/05/150319_Levis_Store_Berlin_167.jpg" />
